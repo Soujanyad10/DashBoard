1b721b2f91a36fd8601c0babf982a9a93c7b0ea7
Soujanya <soujanyad2002@gmail.com>
2022-12-20 22:24:36 +0530

    made changes

--- src/App.js
+++ src/App.js
@@ -105,7 +105,7 @@ const App = () => {
 
                 {/* Apps */}
                 <Route path='/calendar' element={<Calendar/>}/>
-                <Route path='/task' element={<Task/>}/>
+                {/* <Route path='/task' element={<Task/>}/> */}
 
                 {/* Charts */}
                 <Route path='/line' element={<Line/>}/>
